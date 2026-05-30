670f885f08c9a034df515edb5f9baea30f45bf23
Yusuf <youssefmmagdy55@gmail.com>
2026-05-30 12:00:26 +0200

    fix(wallet): fixed test errors (13000892)

--- wallet-service/src/main/java/com/team01/freelance/wallet/WalletServiceApplication.java
+++ wallet-service/src/main/java/com/team01/freelance/wallet/WalletServiceApplication.java
@@ -5,13 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
 @SpringBootApplication(scanBasePackages = {
         "com.team01.freelance.wallet"
 })
 @EnableCaching
-@EnableMongoRepositories(basePackages = "com.team01.freelance.wallet")
 @EnableJpaRepositories(basePackages = {
         "com.team01.freelance.wallet",
         "com.team01.freelance.contract",
--- wallet-service/src/main/java/com/team01/freelance/wallet/config/RedisConfig.java
+++ wallet-service/src/main/java/com/team01/freelance/wallet/config/RedisConfig.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@org.springframework.context.annotation.Profile("!test")
 public class RedisConfig {
 
     @Bean
