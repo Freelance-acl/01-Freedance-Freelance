e66a2aea10f6e7a2f5926e79f7a8748a555193c4
Hamza Elewa <Hamza.Elewa@gmail.com>
2026-06-04 23:15:05 +0200

    chore(job): provide simple CacheManager for tests (22001409)

--- job-service/src/main/java/com/team01/freelance/job/config/JobCacheConfig.java
+++ job-service/src/main/java/com/team01/freelance/job/config/JobCacheConfig.java
@@ -2,8 +2,19 @@ package com.team01.freelance.job.config;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import java.util.List;
 
 @Configuration
 @EnableCaching
 public class JobCacheConfig {
+	@Bean
+	public CacheManager cacheManager() {
+		SimpleCacheManager manager = new SimpleCacheManager();
+		manager.setCaches(List.of(new ConcurrentMapCache("jobDashboard")));
+		return manager;
+	}
 }
